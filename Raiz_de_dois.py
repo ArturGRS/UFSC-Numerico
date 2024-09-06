aac7ba0be76c9839d3190b6757dcac3b2edd83a2
Artur Gemaque <166662528+ArturGRS@users.noreply.github.com>
2024-09-05 22:38:51 -0300

    Create Raiz_de_dois.py

---
+++ Raiz_de_dois.py
@@ -0,0 +1,24 @@
+Valor_1 = 1
+Valor_2 = 2
+contador = 0
+
+while ( contador < 50 ):
+
+    media = (Valor_1 + Valor_2)/2
+
+    if min(media*media, Valor_1*Valor_1) < 2 < max(media*media, Valor_1*Valor_1):
+
+        Valor_1 = Valor_1
+        Valor_2 = media
+
+    else:
+
+        Valor_2 = Valor_2
+        Valor_1 = media
+
+    contador = contador + 1
+    print(media)
+
+# Só para brincar um pouco mais eu quero gerar um tabela para colocar os valores de (Valor_1 e Valor_2 e Media)
+
+
